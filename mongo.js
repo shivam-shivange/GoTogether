ad485d73cc811e13bf7e0cc694ca8b93f362e23c
shivam-shivange <shivamshivange@gmail.com>
2025-08-16 15:59:03 +0530

    keeping only HTML files

--- mongo.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectMongo = async () => {
-  await mongoose.connect(process.env.MONGO_URI, { autoIndex: true });
-  console.log("✅ MongoDB connected");
-};
